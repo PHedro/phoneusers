58bd675b8959d8ea75828688ee1074c900bd7d75
PHedro <pedrohenrique.reis@gmail.com>
2016-10-30 16:18:23 -0200

    email unique

--- user/models.py
+++ user/models.py
@@ -3,6 +3,8 @@ from django.db import models
 
 
 class ConcreteUser(AbstractUser):
+    email = models.EmailField(blank=False, unique=True)
+
     created_at = models.DateTimeField(
         auto_now_add=True,
         null=False,
